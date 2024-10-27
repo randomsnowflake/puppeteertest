8119574639a85e707590d9cd9a0feaac53e24373
Johannes Leimbach <johannes@Johanness-MacBook-Pro.local>
2024-10-27 10:16:55 +0100

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #
+  config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'dummy_secret_key_base'
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
